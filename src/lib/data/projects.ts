1aeb7b755a07c552f95d7913748dd16f8189b1d6
Andres Del Rio <addelrio11@gmail.com>
2024-07-20 20:24:39 -0400

    screenshots added to projects

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -20,7 +20,13 @@ export const items: Array<Project> = [
 			to: new Date("2021-03-08")
 		},
 		skills: getSkills('reactjs', 'css', 'js', 'firebase', 'mui'),
-		type: 'Website App'
+		type: 'Website App',
+		screenshots: [
+			{
+				label: 'Desktop view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Ftwitter-clone%2Ftwitter-clone-1.jpg?alt=media&token=7a41d5c8-12f2-4b82-bb02-edd00fb5e4d7'
+			},
+		]
 	},
 	{
 		slug: 'tesla-clone',
@@ -40,32 +46,24 @@ export const items: Array<Project> = [
 		},
 		skills: getSkills('reactjs', 'css', 'redux', 'ts', 'js'),
 		type: 'Website App',
-		/* screenshots: [
+		screenshots: [
 			{
-				label: 'screen 1',
-				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+				label: 'Desktop view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Ftesla-clone%2Ftwitter-clone-1.jpg?alt=media&token=b154c8b5-7883-4c35-a8c6-4d2aa9dde5ee'
 			},
 			{
-				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+				label: 'Desktop view menu',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Ftesla-clone%2Ftwitter-clone-3.jpg?alt=media&token=7983b5c0-c982-4aa7-a022-3c0bb9e79c56'
 			},
 			{
-				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+				label: 'Mobile view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Ftesla-clone%2Ftwitter-clone-2.jpg?alt=media&token=b13e9218-08ae-44b4-96a5-11a310298242'
 			},
 			{
-				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+				label: 'Mobile view menu',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Ftesla-clone%2Ftwitter-clone-4.jpg?alt=media&token=61666f99-ae6d-4851-94b9-b1aa3b6eb059'
 			}
-		]*/
+		]
 	},
 	{
 		slug: 'crypto-app',
@@ -83,8 +81,22 @@ export const items: Array<Project> = [
 			from: new Date("2022-06-29"),
 			to: new Date("2022-07-06")
 		},
-		skills: getSkills('reactjs', 'ts', 'css', 'js'),
-		type: 'Website App'
+		skills: getSkills('reactjs', 'ts', 'css', 'js', 'redux'),
+		type: 'Website App',
+		screenshots: [
+			{
+				label: 'Desktop view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Fcrypto-app%2Fcrypto-app-1.jpg?alt=media&token=aa9cabde-fbdd-4a05-986c-333e886518db'
+			},
+			{
+				label: 'Desktop view menu',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Fcrypto-app%2Fcrypto-app-2.jpg?alt=media&token=f8cb2917-be02-480f-b14a-4ab1e91f6445'
+			},
+			{
+				label: 'Mobile view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Fcrypto-app%2Fcrypto-app-3.jpg?alt=media&token=c0490271-97c8-478c-abb7-e3aa633f7db5'
+			}
+		]
 	},
 	{
 		slug: 'article-preview-component-master',
@@ -103,7 +115,17 @@ export const items: Array<Project> = [
 			to: new Date("2024-04-11")
 		},
 		skills: getSkills('js', 'html', 'css'),
-		type: 'Component'
+		type: 'Component',
+		screenshots: [
+			{
+				label: 'Desktop view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Farticle-preview-component%2Farticle-preview-component-1.jpg?alt=media&token=455533e7-e08d-48bd-9869-07aca12ee2b0'
+			},
+			{
+				label: 'Mobile view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Farticle-preview-component%2Farticle-preview-component-2.jpg?alt=media&token=168e98dd-58ec-4cc0-b5fb-c4ef667d92a4'
+			},
+		]
 	},
 	{
 		slug: 'newsletter-sign-up-with-success-message-main',
@@ -141,7 +163,14 @@ export const items: Array<Project> = [
 			to: new Date("2024-02-04")
 		},
 		skills: getSkills('html', 'css'),
-		type: 'Email'
+		type: 'Email',
+		screenshots: [
+			{
+				label: 'Desktop view',
+				src: 'https://firebasestorage.googleapis.com/v0/b/portfolio-svelte-3d3e0.appspot.com/o/GitHub%20Storage%2Fnewsletter-email%2Fnewsletter-email.jpg?alt=media&token=0d98bb8b-7d0e-4f17-877d-75e5d6c58701'
+			},
+		]
+		
 	},
 ];
 
